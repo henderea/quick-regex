e3018bd090873367f09aff05c3084e2a69f75a52
Eric Henderson <henderea@gmail.com>
2019-04-09 15:35:46 -0400

    initial version

---
+++ src/index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+
+const options = yargs
+    .string('match')
+    .alias('match', 'm')
+    .alias('match', 'pattern')
+    .alias('match', 'p')
+    .nargs('match', 1)
+    .string('replace')
+    .alias('replace', 'r')
+    .alias('replace', 'sub')
+    .alias('replace', 's')
+    .nargs('replace', 1)
+    .boolean('no-case')
+    .alias('no-case', 'i')
+    .argv;
+
+const readAll = async (stream, encoding = 'utf8') => {
+    return new Promise((resolve, reject) => {
+        stream.setEncoding(encoding);
+        let rv = '';
+        stream.on('data', chunk => {
+            rv += chunk;
+        });
+        stream.on('end', () => {
+            resolve(rv);
+        });
+        stream.on('error', e => {
+            reject(e);
+        });
+    });
+};
+
+(async () => {
+    let matchRegex = new RegExp(options.match, options.noCase ? 'gi' : 'g');
+    let replaceRegex = /(\\?)\$\{(\d+)\}/g;
+    console.log(options.replace);
+    let input = await readAll(process.stdin);
+    let rv = input.replace(matchRegex, (...args) => {
+        return options.replace.replace(replaceRegex, (m, s, i) => s == '\\' ? `\${${i}}` : (i > args.length - 2 ? m : args[i]));
+    });
+    console.log(rv);
+})();
