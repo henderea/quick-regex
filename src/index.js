9913a38285e691411f237518669edef73e6b4e6f
Eric Henderson <henderea@gmail.com>
2019-07-15 13:42:16 -0400

    add a -t/--test mode

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 
 const arg = require('arg');
 const XRegExp = require('xregexp');
-const { HelpTextMaker } = require('@henderea/simple-colors/helpText');
+const { HelpTextMaker, styles } = require('@henderea/simple-colors/helpText');
+const { green, red, magenta } = styles;
 
 const options = arg(
     {
@@ -19,18 +20,22 @@ const options = arg(
         '--one-line': Boolean,
         '-o': '--one-line',
         '--help': Boolean,
-        '-h': '--help'
+        '-h': '--help',
+        '--test': Boolean,
+        '-t': '--test'
     },
     {
         permissive: true
     }
 );
 
+const ex = magenta.bright;
+
 const helpText = new HelpTextMaker('quick-regex')
     .wrap()
     .title.nl
     .pushWrap(4)
-    .tab.text('A regex replace utility that takes stdin, applies the regex replacement, and prints the result').nl
+    .tab.text('A regex replace utility that takes stdin, applies the regex replacement, and prints the result. Using the ').flag('--test', '-t').text(' flag will switch the mode to a boolean match test.').nl
     .popWrap()
     .nl
     .flags.nl
@@ -38,6 +43,7 @@ const helpText = new HelpTextMaker('quick-regex')
     .dict
     .key.tab.flag('--match', '-m', '--pattern', '-p').value.text('The regex pattern to match').end.nl
     .key.tab.flag('--replace', '-r', '--sub', '-s').value.text('The substitution string').end.nl
+    .key.tab.flag('--test', '-t').value.text('Instead of replacing text, exit with code 0 for a match, or code 1 for no match').end.nl
     .key.tab.flag('--no-case', '-i').value.text('Make the regex pattern case-insensitive').end.nl
     .key.tab.flag('--one-line', '-o').value.text('Make the ^ and $ match the beginning and end of the entire input').end.nl
     .key.tab.flag('--help', '-h').value.text('Print this help').end.nl
@@ -56,19 +62,19 @@ const helpText = new HelpTextMaker('quick-regex')
     .pushWrap(8)
     .dict
     .tab.tab.bold('Basic Syntax:').nl
-    .key.tab.tab.tab.text('${1}').value.text('insert capture group 1').end.nl
-    .key.tab.tab.tab.text('${1|2}').value.text("insert capture group 1, or if that didn't match, insert capture group 2; the | syntax can be used in all other syntax setups").end.nl
+    .key.tab.tab.tab.text(ex('${1}')).value.text('insert capture group 1').end.nl
+    .key.tab.tab.tab.text(ex('${1|2}')).value.text("insert capture group 1, or if that didn't match, insert capture group 2; the | syntax can be used in all other syntax setups").end.nl
     .nl
     .tab.tab.bold('Ternary syntax:').nl
-    .key.tab.tab.tab.text('${1?a:b}').value.text("insert either 'a' or 'b', depending on if capture group 1 matched anything; a and b can be empty, and can contain any characters other than : and }").end.nl
-    .key.tab.tab.tab.text('${1?a}').value.text("insert 'a' if capture group 1 matched anything, or insert nothing if it did nto match").end.nl
+    .key.tab.tab.tab.text(ex('${1?a:b}')).value.text("insert either 'a' or 'b', depending on if capture group 1 matched anything; a and b can be empty, and can contain any characters other than : and }").end.nl
+    .key.tab.tab.tab.text(ex('${1?a}')).value.text("insert 'a' if capture group 1 matched anything, or insert nothing if it did nto match").end.nl
     .nl
     .tab.tab.bold('Fallback syntax:').nl
-    .key.tab.tab.tab.text('${1:-a}').value.text("insert capture group 1, or if that didn't match, insert 'a'").end.nl
+    .key.tab.tab.tab.text(ex('${1:-a}')).value.text("insert capture group 1, or if that didn't match, insert 'a'").end.nl
     .nl
     .tab.tab.bold('Substring syntax:').nl
-    .key.tab.tab.tab.text('${1:1}').value.text('insert capture group 1, starting at index 1').end.nl
-    .key.tab.tab.tab.text('${1:1:2}').value.text('insert capture group 1, starting at index 1, with a length of 2').end.nl
+    .key.tab.tab.tab.text(ex('${1:1}')).value.text('insert capture group 1, starting at index 1').end.nl
+    .key.tab.tab.tab.text(ex('${1:1:2}')).value.text('insert capture group 1, starting at index 1, with a length of 2').end.nl
     .endDict
     .popWrap()
     .nl
@@ -78,9 +84,9 @@ const helpText = new HelpTextMaker('quick-regex')
     .nl
     .dict
     .tab.tab.bold('Examples:').nl
-    .key.tab.tab.tab.text('${1?${2}${3}}').value.text("insert capture group 2 if group 1 was matched, or group 3 if it didn't").end.nl
-    .key.tab.tab.tab.text('${1:-${2}}').value.text("same as ${1|2}").end.nl
-    .key.tab.tab.tab.text('${1:${2?1:2}:${2?3:2}}').value.text("insert a substring of capture group 1; if capture group 2 matched, the substring will be 3 characters starting at index 1; if capture group 2 did not match, the substring will be 2 characters starting at index 2").end.nl
+    .key.tab.tab.tab.text(ex('${1?${2}${3}}')).value.text("insert capture group 2 if group 1 was matched, or group 3 if it didn't").end.nl
+    .key.tab.tab.tab.text(ex('${1:-${2}}')).value.text("same as ").text(ex('${1|2}')).end.nl
+    .key.tab.tab.tab.text(ex('${1:${2?1:2}:${2?3:2}}')).value.text("insert a substring of capture group 1; if capture group 2 matched, the substring will be 3 characters starting at index 1; if capture group 2 did not match, the substring will be 2 characters starting at index 2").end.nl
     .endDict
     .popWrap()
     .popWrap()
@@ -189,8 +195,19 @@ let processReplace = (args, replaceString, left = null, right = null) => {
 
 (async () => {
     let matchRegex = new RegExp(options['--match'], `g${options['--no-case'] ? 'i' : ''}${options['--one-line'] ? '' : 'm'}`);
-    let replaceString = options['--replace'];
     let input = await readAll(process.stdin);
-    let rv = input.replace(matchRegex, (...args) => processReplace(args, replaceString));
-    process.stdout.write(rv);
+    if(options['--test']) {
+        let matches = matchRegex.test(input);
+        if(matches) {
+            console.log(green.bright('Match'));
+            process.exit(0);
+        } else {
+            console.log(red.bright('No Match'));
+            process.exit(1);
+        }
+    } else {
+        let replaceString = options['--replace'];
+        let rv = input.replace(matchRegex, (...args) => processReplace(args, replaceString));
+        process.stdout.write(rv);
+    }
 })();
