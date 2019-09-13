e53fb5554e7aa419109d3f2e343214f4d2eb613b
Eric Henderson <henderea@gmail.com>
2019-09-13 16:43:53 -0400

    add support for multiple match/replace sets with -s/--subs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { argParser } = require('@henderea/arg-helper')(require('arg'));
-const { processReplace } = require('../lib/matching');
+const { doMultiReplace } = require('@henderea/regex-util');
 const { readAll, readLines } = require('../lib/readInput');
 const { helpText, styles } = require('../lib/helpText');
 const { red, green } = styles;
@@ -10,6 +10,7 @@ const options = argParser()
     .string('match', '--match', '-m')
     .string('replace', '--replace', '-r')
     .string('input', '--input', '--query', '-q')
+    .strings('subs', '--subs', '-s')
     .bool('help', '--help', '-h')
     .bool('noCase', '--no-case', '-i')
     .bool('oneLine', '--one-line', '-o')
@@ -23,8 +24,28 @@ const options = argParser()
     .argv;
 
 (async () => {
-    let matchRegex = new RegExp(options.match, `g${options.noCase ? 'i' : ''}${options.oneLine ? '' : 'm'}`);
+    let subs = [];
+    let matchRegex = null;
+    if(options.match) {
+        matchRegex = new RegExp(options.match, `g${options.noCase ? 'i' : ''}${options.oneLine ? '' : 'm'}`);
+        if(options.replace) {
+            subs.push({ matchRegex, replaceString: options.replace });
+        }
+    }
+    if(options.subs && Array.isArray(options.subs)) {
+        options.subs.forEach(sub => {
+            let parts = sub.split(/[|]{3}/, 2);
+            let match = new RegExp(parts[0], `g${options.noCase ? 'i' : ''}${options.oneLine ? '' : 'm'}`);
+            let replace = parts[1];
+            subs.push({ matchRegex: match, replaceString: replace });
+        });
+    }
     if(options.grep || options.reverseGrep) {
+        if(!matchRegex) {
+            console.error(`You must provide -m or --match in ${options.reverseGrep ? 'reverse-' : ''}grep mode.`)
+            process.exit(1);
+            return;
+        }
         let processLines = (lines) => {
             let outputLines = lines.filter(line => (matchRegex.test(line) || matchRegex.test(line.replace(/\r?\n$/, ''))) ? !options.reverseGrep : options.reverseGrep).join('');
             if(outputLines.length > 0) {
@@ -34,11 +55,10 @@ const options = argParser()
         await readLines(process.stdin, processLines);
         process.exit(0);
     } else {
-        let replaceString = options.replace;
         let input = options.input;
         let processLines = (lines) => {
             lines.forEach(input => {
-                let rv = input.replace(matchRegex, (...args) => processReplace(args, replaceString));
+                let rv = doMultiReplace(input, subs);
                 if(options.format) {
                     rv = rv.replace(/\\e/g, '\u001b');
                 }
@@ -52,11 +72,21 @@ const options = argParser()
             if(options.oneLine || options.test || !options.stream) {
                 input = await readAll(process.stdin);
             } else {
+                if(subs.length == 0) {
+                    console.error('You must provide -m/--match and -r/--replace, or -s/--subs, in the default replace mode.')
+                    process.exit(1);
+                    return;
+                }
                 await readLines(process.stdin, processLines);
                 process.exit(0);
             }
         }
         if(options.test) {
+            if(!matchRegex) {
+                console.error('You must provide -m or --match in test mode.')
+                process.exit(1);
+                return;
+            }
             let matches = matchRegex.test(input);
             if(matches) {
                 console.log(green.bright('Match'));
@@ -66,6 +96,11 @@ const options = argParser()
                 process.exit(1);
             }
         } else {
+            if(subs.length == 0) {
+                console.error('You must provide -m/--match and -r/--replace, or -s/--subs, in the default replace mode.')
+                process.exit(1);
+                return;
+            }
             processLines([input]);
         }
     }
